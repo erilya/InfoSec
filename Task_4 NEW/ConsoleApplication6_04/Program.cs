e53a417af1a5ef71a673b278bc3ba3bba0e85bd4
erilya <45204131+erilya@users.noreply.github.com>
2020-04-10 11:10:45 +0300

    Add new Task 4

---
+++ Task_4 NEW/ConsoleApplication6_04/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+
+namespace ConsoleApplication6_04
+{
+    class Program
+    {
+
+        static String codes2string(String str)
+        {
+            StringBuilder text = new StringBuilder();
+            Byte[] bytes = new Byte[str.Length / 2];
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                String code = str.Substring(2 * i, 2);
+                bytes[i] = Byte.Parse(code);
+            }
+            return ASCIIEncoding.ASCII.GetString(bytes);
+        }
+
+
+        private static long calculateD(long e, long phi)
+        {
+            long tmp = 1;
+            while (true)
+            {
+                tmp = tmp + phi;
+                if (tmp % e == 0)
+                    return tmp / e;
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            long e = 23311;
+            long n = 274611845366113;
+            long p = 2432321;
+            long q = 112901153;
+            long phi = (p - 1) * (q - 1);
+            Console.WriteLine("phi = {0}", phi);
+            long d = calculateD(e, phi);
+            Console.WriteLine("{0}", (d * e) % phi);
+            String[] c_text = "108230462382949 240744446393133 139920760825242 128635453394626 156290136879344".Split(' ');
+            StringBuilder text = new StringBuilder();
+            StringBuilder c_text_ = new StringBuilder();
+            foreach (String c_str in c_text)
+            {
+                BigInteger c_msg = ulong.Parse(c_str);
+                BigInteger msg = BigInteger.ModPow(c_msg, d, n);
+                text.Append(msg.ToString());
+
+                BigInteger c_msg_ = BigInteger.ModPow(msg, e, n);
+                c_text_.Append(c_msg_.ToString());
+            }
+            Console.WriteLine(c_text_);
+
+            Console.WriteLine(codes2string(text.ToString()));
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
---
+++ Task_4 NEW/ConsoleApplication6_04/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleApplication6_04")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApplication6_04")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7042d11a-c110-4ba1-84fd-e0ea62c8a9a4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
